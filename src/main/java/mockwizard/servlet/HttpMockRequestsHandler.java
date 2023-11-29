cd1b35ceb4c649b90ab64fbab9c5626c6bb765b6
Andre <andrechaumetcutuli@gmail.com>
2023-11-29 10:08:26 -0300

    minor fix, added TODOs

--- src/main/java/mockwizard/servlet/HttpMockRequestsHandler.java
+++ src/main/java/mockwizard/servlet/HttpMockRequestsHandler.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static mockwizard.servlet.RequestFactory.convertToRequestModel;
 
@@ -20,34 +22,43 @@ public class HttpMockRequestsHandler implements com.sun.net.httpserver.HttpHandl
 
     //TODO: Log
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpMockRequestsHandler.class);
+    //TODO: remove hardcoded, configure properties
+    private static final Integer POOL_SIZE = 20;
 
     private final MockService service;
+    private final ExecutorService executorService;
+
 
     @Autowired
     public HttpMockRequestsHandler(MockService mockService) {
         this.service = mockService;
+        this.executorService = Executors.newFixedThreadPool(POOL_SIZE);
     }
 
+
     @Override
-    public void handle(HttpExchange exchange) {
-        new Thread(() -> {
+    public void handle(final HttpExchange exchange) {
+        executorService.submit(() -> {
             try {
                 handleAsync(exchange);
-                //TODO: exception handling, refactor, create metrics & save error data as file
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
+            } catch (Exception e) {
+                //TODO: exception handling, refactor, log, create metrics & save error data as file
+                LOGGER.error("");
+                e.printStackTrace();
             }
-        }).start();
+        });
     }
 
     //TODO: Store in the mock file how much delay I want the response to return.
-    private void handleAsync(final HttpExchange exchange) throws IOException, InterruptedException {
+    private void handleAsync(final HttpExchange exchange) throws IOException {
         try {
             handleResponse(exchange, handleRequest(exchange));
-        } catch (HttpMockWizardException e) {
+        } catch (final HttpMockWizardException e) {
+            //TODO: create class/factory for this
             exchange.sendResponseHeaders(e.getCode(), e.getMessage().length());
             exchange.getResponseBody().write(e.getMessage().getBytes());
-        } catch (IOException e) {
+        } catch (final IOException e) {
+            //TODO:
             exchange.sendResponseHeaders(500, 0);
         } finally {
             exchange.close();
