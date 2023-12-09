9cf207f2d029e52f660aede02043c61a790e1874
Andre <andrechaumetcutuli@gmail.com>
2023-12-09 15:07:22 -0300

    checkpoint

--- src/main/java/mockwizard/App.java
+++ src/main/java/mockwizard/App.java
@@ -5,17 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class App {
 
-    //TODO: Used for instance managing, fix
     private final MockConfig mockConfig;
 
     @Autowired
     public App(MockConfig mockConfig) {
         this.mockConfig = mockConfig;
+        this.mockConfig.init();
     }
 
     public static void main(String[] args) {
--- src/main/java/mockwizard/config/MockConfig.java
+++ src/main/java/mockwizard/config/MockConfig.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.annotation.PreDestroy;
 import java.io.IOException;
@@ -18,7 +17,7 @@ import java.net.InetSocketAddress;
 public class MockConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MockConfig.class);
-    //TODO: Properties
+
     private static final Integer PORT = 8088;
     private static final Integer DEFAULT_BACKLOG = -1;
     private static final Integer SHUTTING_DELAY = 0;
@@ -28,13 +27,11 @@ public class MockConfig {
     private final HttpServer server;
 
     @Autowired
-    public MockConfig(HttpMockServlet httpMockServlet, HttpServer server) throws IOException {
+    public MockConfig(HttpMockServlet httpMockServlet) throws IOException {
         this.httpMockServlet = httpMockServlet;
         this.server = HttpServer.create(new InetSocketAddress(PORT), DEFAULT_BACKLOG);
     }
 
-    //TODO: hotfix solve later
-    @Scheduled(initialDelayString = "1000", fixedRate = Long.MAX_VALUE)
     public void init() {
         server.createContext(BASE_PATH, httpMockServlet);
         server.start();
