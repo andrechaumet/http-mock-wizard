61a42b371ea9c686d626f34982f1335aaf6b7640
Andre <andrechaumetcutuli@gmail.com>
2023-12-11 08:14:55 -0300

    checkpoint

--- src/main/java/mockwizard/persistence/MocksRepository.java
+++ src/main/java/mockwizard/persistence/MocksRepository.java
@@ -1,4 +1,4 @@
-package mockwizard.repository;
+package mockwizard.persistence;
 
 import mockwizard.model.Mock;
 import mockwizard.model.ReadOnlyMock;
@@ -7,12 +7,12 @@ import com.querydsl.core.types.Predicate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.io.IOException;
 import java.util.Optional;
 
 public interface MocksRepository {
     Optional<ReadOnlyMock> findByUriAndMethod(final String uri, final String method);
     Page<ReadOnlyMock> findAll(final Pageable pageable, final Predicate predicate);
-    ReadOnlyMock save(final Mock mock) throws IOException;
-    void delete(final String uri, final String method) throws IOException;
+    ReadOnlyMock save(final Mock mock);
+    void delete(final String uri, final String method);
+    void deleteAllByUri(final String uri);
 }
